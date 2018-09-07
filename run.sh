edbe498ef68f9a934048fefa484fc626d8884ad9
Farasath Ahamed <farasatha@wso2.com>
2018-09-07 17:36:47 +0530

    * Add README.md and README.txt * Remove unwanted features to speedup build

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-mvn clean install
+IS_SOURCE_HOME=/home/farazath/IS/product-is
+IS_VERSION=wso2is-5.7.0-beta
+
+mvn clean install -Dmaven.test.skip=true
 cd /tmp
 rm -rf wso2is-5.7.0-beta
 
-unzip /home/farazath/IS/product-is/modules/distribution/target/wso2is-5.7.0-beta.zip
-cd wso2is-5.7.0-beta
+unzip $IS_SOURCE_HOME/modules/distribution/target/$IS_VERSION.zip
+cd $IS_VERSION
 cd bin
 sh wso2server.sh -DosgiConsole
