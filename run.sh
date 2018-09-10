fd03419f4c6cb825bed4fd51dbf3b12faab8f508
Farasath Ahamed <farasatha@wso2.com>
2018-09-10 12:31:26 +0530

    Update features to be par with IS 5.7.0-rc1

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 IS_SOURCE_HOME=/home/farazath/IS/product-is
-IS_VERSION=wso2is-5.7.0-beta
+IS_VERSION=wso2is-5.7.0
 
 mvn clean install -Dmaven.test.skip=true
 cd /tmp
-rm -rf wso2is-5.7.0-beta
+rm -rf $IS_VERSION.
 
 unzip $IS_SOURCE_HOME/modules/distribution/target/$IS_VERSION.zip
 cd $IS_VERSION
